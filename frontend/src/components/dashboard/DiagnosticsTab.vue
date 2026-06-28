6d88d43034afd058982a2eea3e8fb35ee34f3162
spcfranz-rgb <spcfranz@gmail.com>
2026-06-28 13:48:16 -0400

    Update DiagnosticsTab.vue

--- frontend/src/components/dashboard/DiagnosticsTab.vue
+++ frontend/src/components/dashboard/DiagnosticsTab.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="row g-4">
-    <div class="col-lg-6">
+    
+    <div v-if="canRunDiagnostics" class="col-lg-6">
       <PingTraceTool />
     </div>
-    <div class="col-lg-6">
+    
+    <div :class="canRunDiagnostics ? 'col-lg-6' : 'col-12'">
       <SpeedtestTool />
     </div>
+
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import PingTraceTool from '../diagnostics/PingTraceTool.vue'
 import SpeedtestTool from '../diagnostics/SpeedtestTool.vue'
+import { useSystemStore } from '../../stores/systemStore'
+
+const store = useSystemStore()
+
+// RBAC: Only admins and operators can access active network probes
+const canRunDiagnostics = computed(() => {
+  return ['admin', 'operator'].includes(store.user?.role)
+})
 </script>
