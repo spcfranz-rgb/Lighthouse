62bd4cc1e5741adf1f73ce1f449f1d748e32ffce
spcfranz-rgb <spcfranz@gmail.com>
2026-06-28 15:00:05 -0400

    Update DashboardView.vue

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -26,15 +26,32 @@
 
       <div class="row mb-4">
         <div class="col-lg-6 mb-4 mb-lg-0">
-          <DeviceTable title="Network Switches" type="switches" :devices="store.devices.switches" />
+          <DeviceTable 
+            title="Network Switches" 
+            type="switches" 
+            :devices="store.devices.switches" 
+            @edit="openEdit" 
+          />
         </div>
         <div class="col-lg-6">
-          <DeviceTable title="Network Video Recorders" type="nvrs" :devices="store.devices.nvrs" />
+          <DeviceTable 
+            title="Network Video Recorders" 
+            type="nvrs" 
+            :devices="store.devices.nvrs" 
+            @edit="openEdit" 
+          />
         </div>
       </div>
+      
       <div class="row">
         <div class="col-12">
-          <DeviceTable title="Cameras" type="cameras" :devices="store.devices.cameras" @preview="openPreview" />
+          <DeviceTable 
+            title="Cameras" 
+            type="cameras" 
+            :devices="store.devices.cameras" 
+            @preview="openPreview" 
+            @edit="openEdit" 
+          />
         </div>
       </div>
     </div>
@@ -47,39 +64,73 @@
       <AdminTab />
     </div>
 
-    <WebRtcPreviewModal v-if="previewCam" :camera="previewCam" @close="previewCam = null" />
-    <DeviceAddModal v-if="showAddModal" @close="showAddModal = false" />
-    <NetworkScannerModal v-if="showScannerModal" @close="showScannerModal = false" />
+    <WebRtcPreviewModal 
+      v-if="previewCam" 
+      :camera="previewCam" 
+      @close="previewCam = null" 
+    />
+    
+    <DeviceAddModal 
+      v-if="showAddModal" 
+      @close="showAddModal = false" 
+    />
+    
+    <NetworkScannerModal 
+      v-if="showScannerModal" 
+      @close="showScannerModal = false" 
+    />
+
+    <DeviceEditModal 
+      v-if="editingDevice" 
+      :device="editingDevice" 
+      :type="editingType" 
+      @close="editingDevice = null" 
+    />
 
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+
+// Sub-components
 import TopNav from '../components/layout/TopNav.vue'
 import DeviceTable from '../components/monitoring/DeviceTable.vue'
 import DiagnosticsTab from '../components/dashboard/DiagnosticsTab.vue'
 import AdminTab from '../components/dashboard/AdminTab.vue'
 
+// Modals
 import WebRtcPreviewModal from '../components/modals/WebRtcPreviewModal.vue'
 import DeviceAddModal from '../components/modals/DeviceAddModal.vue'
 import NetworkScannerModal from '../components/modals/NetworkScannerModal.vue'
+import DeviceEditModal from '../components/modals/DeviceEditModal.vue'
 
+// State Management
 import { useSystemStore } from '../stores/systemStore'
 
 const store = useSystemStore()
 const activeTab = ref('monitoring')
 
-// Modal States
+// --- Modal Visibility States ---
 const previewCam = ref(null)
 const showAddModal = ref(false)
 const showScannerModal = ref(false)
+const editingDevice = ref(null)
+const editingType = ref(null)
 
+// --- Lifecycle ---
 onMounted(() => {
+  // Fetch hardware grid, settings, and latest speed test on load
   store.fetchSystemData()
 })
 
+// --- Action Handlers ---
 const openPreview = (cam) => {
   previewCam.value = cam
+}
+
+const openEdit = (device, type) => {
+  editingDevice.value = device
+  editingType.value = type
 }
 </script>
